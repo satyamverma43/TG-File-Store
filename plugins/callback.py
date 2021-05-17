c1063fe557cc0c0ce3076dc912184a8bce24a450
satyamverma43 <69762426+satyamverma43@users.noreply.github.com>
2021-05-17 18:49:24 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -67,12 +67,12 @@ async def about_cb(c, m):
 
 👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
 
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](http://t.me/thesatyamxyz)
 
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](http://t.me/thesatyamxyz)
 
-🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/Ns-Bots/TG-File-Store)
-"""
+🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](http://t.me/thesatyamxyz)
+""
 
     # creating buttons
     buttons = [
